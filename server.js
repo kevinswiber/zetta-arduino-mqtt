daf345d6e0e8518d953f4d922199b422a074363c
Kevin Swiber <kswiber@gmail.com>
2014-12-11 11:30:04 -0500

    Removing Ambient sensor that was used in Tessel since I moved to Arduino.

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 var zetta = require('zetta');
 var MqttScout = require('./mqtt_scout');
-var Ambient = require('./ambient_attx4');
 var LED = require('./led');
 
 var options = {
   uri: process.env.MQTT_URI,
   map: {
-    'ambient-attx4': Ambient,
     'led': LED
   }
 };
 
 zetta()
-  .name('tessel-fun')
+  .name('arduino-mqtt')
   .use(MqttScout, options)
   .listen(1337);
