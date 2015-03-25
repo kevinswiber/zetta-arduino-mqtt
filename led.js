83e64ad4be8535fcef807ed0eef840ddeece2949
Kevin Swiber <kswiber@gmail.com>
2015-03-25 10:59:18 -0400

    Simplifying LED driver.

--- led.js
+++ led.js
@@ -16,40 +16,46 @@ LED.prototype.init = function(config) {
     .state('off')
     .when('off', { allow: ['turn-on'] })
     .when('on', { allow: ['turn-off'] })
-    .when('waiting-turn-on', { allow: ['turn-on-ack'] })
-    .when('waiting-turn-off', { allow: ['turn-off-ack'] })
     .map('turn-on', this.tryTurnOn)
     .map('turn-off', this.tryTurnOff)
     .map('turn-on-ack', this.turnOnAck)
     .map('turn-off-ack', this.turnOffAck);
+    //.when('waiting-turn-on', { allow: ['turn-on-ack'] })
+    //.when('waiting-turn-off', { allow: ['turn-off-ack'] })
+    //.map('turn-on', this.tryTurnOn)
+    //.map('turn-off', this.tryTurnOff)
 };
 
 LED.prototype.tryTurnOn = function(cb) {
   this._client.publish('led/' + this._name + '/transition/turn-on');
-  this.state = 'waiting-turn-on';
+  this.state = 'on';
   cb();
 };
 
 LED.prototype.tryTurnOff = function(cb) {
   this._client.publish('led/' + this._name + '/transition/turn-off');
-  this.state = 'waiting-turn-off';
+  this.state = 'off';
   cb();
 };
 
 LED.prototype.turnOnAck = function(cb) {
-  this.state = 'on';
+  if (this.state !== 'on') {
+    this.state = 'on';
+  }
   cb();
 };
 
 LED.prototype.turnOffAck = function(cb) {
-  this.state = 'off';
+  if (this.state !== 'off') {
+    this.state = 'off';
+  }
   cb();
 };
 
 LED.prototype.ack = function(transition, message) {
   if (transition === 'turn-on') {
-    this.call('turn-on-ack');
+    //this.call('turn-on-ack');
   } else if (transition === 'turn-off') {
-    this.call('turn-off-ack');
+    //this.call('turn-off-ack');
   }
 };
